8847a68019328fdf45d354a9944b4a2271424c25
Napong Keha <gorngorn9876@hotmail.com>
2020-08-13 13:01:47 +0700

    nothing

--- Name/Name.cpp
+++ Name/Name.cpp
@@ -1,5 +1,8 @@
 #include<stdio.h>
 int main() {
-	printf("Hello World");
+	char name[10];
+	char sname[10];
+	scanf_s("%s %s", &name, &sname);
+	printf("Hellow %s %s welcome to CE",name,sname);
 	return 0;
 }
